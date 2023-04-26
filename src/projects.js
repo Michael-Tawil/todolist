d748114dc70a0a0999ff8dfa6eb97aab70b13ff7
michael-tawil <michaeltawil3@gmail.com>
2023-04-26 15:29:18 +1000

    adding functions to projects

---
+++ src/projects.js
@@ -0,0 +1,27 @@
+import './tasks'
+const projects = []
+
+class Project{
+    constructor(name,ddate){
+        this.name = name;
+        this.ddate = ddate;
+        this.tasks = [];
+    }
+    GetName = () => this.name;
+    SetName =(nname) => this.name = nname;
+    AddTask = (task) => this.tasks.push(task);
+
+    RemoveTask = (rtask) => {
+        let temp = this.tasks.findIndex(el => el.task === rtask);
+        this.tasks.splice(temp,1);
+        console.log(temp)
+    }
+
+}
+let G = new Project("General","24/4/25")
+
+G.SetName("khara")
+G.AddTask({task:1})
+G.AddTask({task:12})
+console.log(G.tasks)
+
---
+++ src/tasks.js
@@ -0,0 +1,10 @@
+class Task {
+    constructor(name,ddate,desc){
+        this.name = name;
+        this.ddate = ddate;
+        this.desc = desc;
+    }
+}
+let n = new Task("firstask","34/4/24","blah blah");
+
+console.log(n);
