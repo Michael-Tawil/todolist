5ae327ac4a67c54c6eb15ce3216a1f6d2c2fe92a
michael-tawil <michaeltawil3@gmail.com>
2023-04-26 15:31:11 +1000

    wspace

--- src/projects.js
+++ src/projects.js
@@ -10,18 +10,14 @@ class Project{
     GetName = () => this.name;
     SetName =(nname) => this.name = nname;
     AddTask = (task) => this.tasks.push(task);
-
     RemoveTask = (rtask) => {
         let temp = this.tasks.findIndex(el => el.task === rtask);
         this.tasks.splice(temp,1);
-        console.log(temp)
     }
 
 }
 let G = new Project("General","24/4/25")
-
 G.SetName("khara")
 G.AddTask({task:1})
 G.AddTask({task:12})
 console.log(G.tasks)
-
